cfddeeffa883346f5ad247b3b3dcd91746320329
Gaurav Agarwal <gagarwal003@gmail.com>
2024-05-11 22:12:51 -0700

    Updated getDisplayName.py
    added support for themes and genres

--- getDisplayName.py
+++ getDisplayName.py
@@ -1,9 +1,37 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup, SoupStrainer
+import ast
+
 
 requestsSession = requests.Session()
+def has(themes_list, target_string):
+    return any(target_string.lower() in theme.lower() for theme in themes_list)
+
+def getDisplayNameThemeNanoGenre(name, columnLabel, iD):
+    df = pd.read_csv('movies.csv')
+    df[columnLabel] = df[columnLabel].apply(ast.literal_eval)
+    movieID = None
+    for index, row in df.iterrows():
+        if has(row[columnLabel], iD):
+            movieID = row['movieID']
+    if movieID:
+        response = requestsSession.get(f"https://letterboxd.com/film/{movieID}/{name}s/")
+        soup = BeautifulSoup(response.text, 'lxml', parse_only=SoupStrainer("a"))
+        title = soup.find(href=lambda href: href and href.startswith(f"/films{iD}"))   
+        return title.find('span').get_text()
+    
 
 def getDisplayName(iD):
+    if not isinstance(iD, str):
+        return iD
+    
+    if iD.startswith("/theme/"):
+        return getDisplayNameThemeNanoGenre('theme', 'themes', iD)
+    
+    if iD.startswith("/nanogenre/"):
+        return getDisplayNameThemeNanoGenre('nanogenre', 'nanoGenres', iD)
+    
     if iD.startswith("/language/"):
         response = requestsSession.get(f"https://letterboxd.com/countries/")
         soup = BeautifulSoup(response.text, 'lxml')
